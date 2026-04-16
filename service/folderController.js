b7de8896dd92953fe75cf017ce4c81006d343246
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-04-16 15:54:34 +0300

    20260416115242_relation_between_users_and_folders

--- service/folderController.js
+++ service/folderController.js
@@ -1,27 +1,39 @@
+import { body, matchedData, validationResult } from 'express-validator';
 import { prisma } from '../ORM/lib/prisma.js';
 
 export const createFolderGet = async (req, res) => {
     try {
-        res.render('create_folder');
+        const folders = await prisma.folder.findMany(
+            {
+                where: { userId: req.user.id },
+                select: { name: true }
+            }
+        );
+        console.log(folders);
+        res.render('create_folder', { folders: folders });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "couldn't vew folder form error" });
     }
 };
-export const createFolderPost = async (req, res) => {
-    const name = req.body.folder;
-    try {
-        await prisma.folder.create({
-            data: {
-                name: name,
-            }
-        });
-        res.render("/folder");
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "couldn't create folder error" });
+export const createFolderPost = [
+    async (req, res) => {
+        try {
+            const name = req.body.folder;
+            const folder = await prisma.folder.create({
+                data: {
+                    name: name,
+                    userId: req.user.id
+                }
+            });
+            res.redirect(`/folder/${folder.id}`);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "couldn't create folder error" });
+        }
     }
-};
+];
+
 export const folderContentGet = async (req, res) => {
     const { id } = req.params;
     try {
@@ -30,6 +42,7 @@ export const folderContentGet = async (req, res) => {
             select: { files: true, id: true, name: true }
 
         });
+        console.log(folder);
         res.render("folder_files", { folder: folder });
     } catch (error) {
         console.error(error);
