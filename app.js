d8b04df7b8867640cee61586b338eb77deefc73b
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-18 19:30:54 +0300

    add health endpoint

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Backend running on http://localhost:${PORT}/`);
 });
-
+app.get('/health', (req, res) => {
+    res.send('ok');
+});
 
 export { app };
